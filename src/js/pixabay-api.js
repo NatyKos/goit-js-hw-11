052eb13ad0b340ce3067500ec588b12f5fb5ede1
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-02-23 21:18:40 +0000

    1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,6 @@
+const KEY = '42530845-4f1978a0628226c655e2788d5';
+const BASE_URL = 'https://pixabay.com/api/';
+const form = document.querySelector('.form');
+
+// value.trim().replace(/\s/g, "+");
+// console.log(query);
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/pixabay-api';
+import './js/render-function'
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
